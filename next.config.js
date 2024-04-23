c09f11858d9336d40896bb151e814a1110545ab6
Riyaj Hannure <developerhannure@gmail.com>
2024-04-23 17:43:18 +0530

    23Apr24

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // experimental: {
-    //     missingSuspenseWithCSRBailout: false,
-    // },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     images: {
         unoptimized: true,
         domains: ['giinfotech.ae','static.giinfotech.ae','tripadvisor.com'],
